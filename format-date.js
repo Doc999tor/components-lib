b437374fb3497313fe7142d7d1816ea634c4d215
vipslord <my.yershov@gmail.com>
2017-11-07 14:38:40 +0200

    update components

--- format-date.js
+++ format-date.js
@@ -1,2 +1,1 @@
-export default d =>
-  moment(d).hours() + ':' + moment(d).format('mm') + ' | ' + moment(d).date() + ' ' + config.translations.dates.months[moment(d).month()]
+export default d => moment(d).hours() + ':' + moment(d).format('mm') + ' | ' + moment(d).date() + ' ' + config.translations.dates.months[moment(d).month()]
