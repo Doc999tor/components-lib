cc59712908e60b5884bd24ed8cc75930672fbf05
vipslord <my.yershov@gmail.com>
2017-11-07 15:21:15 +0200

    update save

--- save.js
+++ save.js
@@ -1,4 +1,3 @@
-import {clientPostService} from 'project-services'
 export default () => {
   let body = new FormData()
   const checkFields = Object.keys(config.urls.fields).map(i => config.urls.fields[i])
@@ -6,11 +5,5 @@ export default () => {
     const bodyUpdated = config.data[i]
     bodyUpdated ? Array.isArray(bodyUpdated) ? bodyUpdated.length > 0 ? body.append(i, JSON.stringify(bodyUpdated)) : '' : body.append(i, bodyUpdated) : ''
   })
-  clientPostService(body).then(r => {
-    if (r.status === 201) {
-      r.json().then(id => {
-        config.data.id = id
-      })
-    }
-  })
+  return body
 }
