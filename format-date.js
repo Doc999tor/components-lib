e87e907c496273a64e919584fa73585fd4518043
vipslord <my.yershov@gmail.com>
2018-03-30 17:37:32 +0300

    Change according to task

--- format-date.js
+++ format-date.js
@@ -1,1 +1,1 @@
-export default d => moment(d).hours() + ':' + moment(d).format('mm') + ' | ' + moment(d).date() + ' ' + config.translations.dates.months[moment(d).month()]
+export default d => moment(d).format('ddd, DD MMM, Y')
