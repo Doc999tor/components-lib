79a5acad75641f15b27a03e955c13d9e97f5619a
vipslord <my.yershov@gmail.com>
2018-05-07 22:27:07 +0300

    update request.service.js

--- request.service.js
+++ request.service.js
@@ -5,12 +5,12 @@ const mainRequestService = (u, o) => {
   o.credentials = 'include'
   return fetch(u, o).then(r => {
     if (r.status === 401) {
-      window.location.href = window.location.origin + '/login'
-      return
+      window.location.href = window.location.origin + config.urls.login
+    } else if (r.status === 503) {
+      return new Promise(resolve => setTimeout(() => resolve(mainRequestService(u, o)), r.headers.get('Retry-After')))
+    } else {
+      return r
     }
-    return r.status === 503
-      ? new Promise(resolve => setTimeout(() => resolve(mainRequestService(u, o)), r.headers.get('Retry-After')))
-      : r
   })
 }
 
