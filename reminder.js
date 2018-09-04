e929839de24996c9ab547f587d7655488b85834e
DenisKenzo <denisamk44@gmail.com>
2018-09-04 17:48:29 +0300

    changed date format

--- reminder.js
+++ reminder.js
@@ -1,1 +1,1 @@
-export default (c, u) => c !== '0' && c !== 0 ? moment.utc().add(c, u).format('YYYY-MM-DD[T]HH:mm:ss.SSS[Z]') : undefined
+export default (c, u) => c !== '0' && c !== 0 ? moment.utc().add(c, u).format('YYYY-MM-DD hh:mm:ss') : undefined
