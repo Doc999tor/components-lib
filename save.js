517170388a1d838b94fe42fa508ed52bcd6169c3
vipslord <my.yershov@gmail.com>
2018-09-13 13:56:23 +0300

    upd save.js

--- save.js
+++ save.js
@@ -4,7 +4,15 @@ export default () => {
   }
   Object.keys(config.urls.fields).forEach(i => {
     if (config.data[i]) {
-      body[i] = config.data[i]
+      if (i === 'depts') {
+        body[i] = JSON.stringify(config.data[i])
+      } else if (i === 'social') {
+        body[i] = JSON.stringify(config.data[i])
+      } else if (i === 'notes') {
+        body[i] = JSON.stringify(config.data[i])
+      } else {
+        body[i] = config.data[i]
+      }
     }
   })
   return encodeURI(Object.keys(body).map(i => `${i}=${body[i]}`).join('&'))
