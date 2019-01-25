3d412814e9b8ac787f919a395fe91b81b647b504
DenisKenzo <denisamk44@gmail.com>
2019-01-25 17:25:33 +0200

    add new DateFormat

--- format-date.js
+++ format-date.js
@@ -1,1 +1,26 @@
-export default d => moment(d).format('ddd, DD MMM, Y')
+export default d => {
+  var date = new Date((d));
+  var options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' }
+  return new Intl.DateTimeFormat(config.locale, options).format(date)
+}
+      // if (config.lang && (config.lang === 'he')) {
+  // } else if (config.lang && (config.lang === 'ru')) {
+  //   return new Intl.DateTimeFormat('ru-RUS', options).format(date)
+  // }
+  // else {
+  //   return new Intl.DateTimeFormat('en-US', options).format(date)
+  // }
+  // return new Intl.DateTimeFormat('en-US', options).format(date)
+
+  // else if (lang === 'ru') {
+  //   return new Intl.DateTimeFormat('ru-RUS', options).format(date)
+  // }
+  // return new Intl.DateTimeFormat('en-US', options).format(date)
+  // if (lang === 'he') {
+  //   return new Intl.DateTimeFormat('he-IL', options).format(date)
+  // } 
+  // else if (lang === 'ru') {
+  //   return new Intl.DateTimeFormat('ru-RUS', options).format(date)
+  // }
+  // moment(d).format('ddd, DD MMM, Y')
+
