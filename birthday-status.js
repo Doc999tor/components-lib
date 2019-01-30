8a78807449652ae6dae0ca7edddb1ba4d10871c9
DenisKenzo <denisamk44@gmail.com>
2019-01-30 13:27:37 +0200

    add fields to translations

--- birthday-status.js
+++ birthday-status.js
@@ -6,6 +6,6 @@ export default d => {
     if (get(d, 'month') + 1 === get(t, 'month')) { dl = -1 * (getEO(d) - get(d, 'date') + get(t, 'date')) } else
     if (get(d, 'month') - 1 === get(t, 'month')) { dl = getEO(t) - get(t, 'date') + get(d, 'date') }
     if (config.translations.dates.days[dl]) { return config.translations.dates.days[dl] } else
-    if (dl < 31 && dl > 1) { return dl + ' days' } else
-    if (dl > -8 && dl < -1) { return (-1 * dl) + ' days ago' }
+    if (dl < 31 && dl > 1) { return dl + ' ' + config.translations.days } else
+    if (dl > -8 && dl < -1) { return (-1 * dl) + config.translations.days_ago }
 }
