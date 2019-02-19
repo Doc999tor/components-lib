18f6a7a06cc9d17e350b71e3f32321d58a96e004
vipslord <my.yershov@gmail.com>
2019-02-19 16:43:49 +0200

    upd Select component

--- Select/Select.jsx
+++ Select/Select.jsx
@@ -14,8 +14,9 @@ export default class Select extends React.Component {
     else arrow = '▼'
     return (
       <div id='select-main' ref='active' tabIndex='1' onBlur={() => this.setState({isVisibleList: false})}>
-        <div className='active' onClick={this.props.disabled ? () => {} : () => this.setState({isVisibleList: true}, () => this.refs.active.focus())}>
-          {this.props.value}<span className='icon'>{arrow}</span>
+        <div className='active' onClick={this.props.disabled ? () => {} : () => this.setState({isVisibleList: !this.state.isVisibleList}, () => this.state.isVisibleList && this.refs.active.focus())}>
+          <div className='active-name'>{this.props.name}</div>
+          <span className='icon'>{arrow}</span>
         </div>
         <div className={this.state.isVisibleList ? 'options' : 'hidden'}>
           {this.props.options.map(i => (<div key={i.label} onClick={() => {
@@ -30,7 +31,7 @@ export default class Select extends React.Component {
 Select.propTypes = {
   onChange: PropTypes.func,
   disabled: PropTypes.bool,
-  value: PropTypes.string,
+  name: PropTypes.string,
   options: PropTypes.arr
 }
 Select.defaultProps = {
