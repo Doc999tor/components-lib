600cfe125b5e35489de7b6fb04b230db8cc83e31
DenisKenzo <denisamk44@gmail.com>
2019-03-01 17:33:46 +0200

    add fields Photo and Permit_ads

--- save.js
+++ save.js
@@ -1,8 +1,11 @@
-export default newPicture => {
+export default (newPicture, newAgreement) => {
   let body = new FormData()
+  let newAgr = newAgreement
+  let newPic = newPicture
   const d = moment().format('YYYY-MM-DD hh:mm:ss')
   body.append('added', d)
-  config.data[config.urls.fields.photo] = newPicture
+  newPicture && body.append('photo', newPic)
+  newAgreement && body.append('permit_ads', newAgr)
   const checkFields = Object.keys(config.urls.fields).map(i => config.urls.fields[i])
   checkFields.forEach(i => {
     const bodyUpdated = config.data[i]
