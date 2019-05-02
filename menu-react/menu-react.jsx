4063c0875b6e99a75e2bfa8dcd10e9a5e388c762
schvvarz <kirillsumovskyi@gmail.com>
2019-05-02 20:38:27 +0300

    scroll off

--- menu-react/menu-react.jsx
+++ menu-react/menu-react.jsx
@@ -1,31 +1,36 @@
 import React from 'react'
 import './menu-react.css'
 
-const MenuReact = ({isMenuOpen, onHandleMenu}) => {
+const MenuReact = ({isMenuOpen, onHandleMenuClose}) => {
+  if (isMenuOpen) {
+    document.body.classList.add('no-scroll')
+  } else {
+    document.body.classList.remove('no-scroll')
+  }
   return (
     <div id='menu_modal'
       className={isMenuOpen ? 'show' : 'show hide'}
-      style={{direction: `${config.data.isRTL ? 'rtl' : 'ltr'}`}}
+      style={{direction: `${window.config.data.isRTL ? 'rtl' : 'ltr'}`}}
       // close menu
-      onClick={() => onHandleMenu()}>
+      onClick={() => onHandleMenuClose()}>
       <div className='menu_wrap'>
         <div className='list-wrap'>
           <div className='menu'>
             <div className='menu-logo'>
               <div className='logo'>
-                <img className='business_logo' src={config.user.business_logo} />
+                <img className='business_logo' src={window.config.user.business_logo} />
               </div>
-              <p className='business_name'>{config.user.business_name}</p>
+              <p className='business_name'>{window.config.user.business_name}</p>
               <p className='business_address'>
-                <img className='business_address_img' src={`${config.urls.menu_icons}map-pin.svg`} />
-                {config.user.business_address}
+                <img className='business_address_img' src={`${window.config.urls.menu_icons}map-pin.svg`} />
+                {window.config.user.business_address}
               </p>
             </div>
-            <nav className='menu-list'>{config.menu.map((item, key) => {
+            <nav className='menu-list'>{window.config.menu.map((item, key) => {
               return (<li className='list-item' key={key}>
                 <a className='item-link' href={item.link}>
-                  <img className='menu-img' src={config.urls.menu_icons + item.icon} alt={item.text} />
-                  {config.translations.menu[item.text]}
+                  <img className='menu-img' src={window.config.urls.menu_icons + item.icon} alt={item.text} />
+                  {window.config.translations.menu[item.text]}
                 </a></li>)
             })}
             </nav>
