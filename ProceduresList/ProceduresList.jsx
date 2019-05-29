4cce4938d0ef811e917ab6ed7aa5417111dbce76
vipslord <my.yershov@gmail.com>
2019-05-29 13:58:54 +0300

    upd search in ProceduresList

--- ProceduresList/ProceduresList.jsx
+++ ProceduresList/ProceduresList.jsx
@@ -1,16 +1,15 @@
 import './ProceduresList.styl'
-
+const getVisibleServices = (services, filter) => services.filter(service => service.name.includes(filter))
 export default class ProceduresList extends React.Component {
   constructor (props) {
     super(props)
     this.services = props.data
     this.state = {
       clickId: '',
+      filter: '',
       showServices: false,
       categories: this.getGroup(props.data),
-      services: this.services,
-      isCategory: false,
-      search: ''
+      services: this.services
     }
   }
   getGroup = a => {
@@ -18,11 +17,8 @@ export default class ProceduresList extends React.Component {
     a.forEach(i => { o[i.category.id] = { ...i.category, count: o[i.category.id] ? o[i.category.id].count + 1 : 1 } })
     return Object.values(o)
   }
-
-  search = e => {
-    if (!this.props.isOpenServices) this.props.toogleOpenServices()
-    let services = e !== '' ? this.services.filter(i => i.name.includes(e) || (i.category && i.category.name.includes(e))) : this.services
-    this.setState({services, search: e, isCategory: true})
+  handleFilterChange = e => {
+    this.setState({ filter: e.target.value })
   }
 
   handleclickCategory = id => {
@@ -32,19 +28,30 @@ export default class ProceduresList extends React.Component {
     })
   }
 
+  renderService = item => (<div className='wrap-service'><div className='service' style={{borderColor: item.color}} onClick={() => this.props.getService(item)}>
+    <h2 className='name'>{item.name}</h2>
+    <div className='edditional-info'>
+      <div className='dur-wrap'><img src={`${config.urls.media}ic_time.svg`} /><p className='duration'>{`${item.duration} ${config.translations.duration_minutes}`}</p></div>
+      <div className='price-wrap'><img src={`${config.urls.media}debt.svg`} /><p className='price'>{`${item.price} ${config.data.currency}`}</p></div>
+    </div>
+  </div>
+  </div>
+  )
+
   render () {
-    const { showServices, clickId } = this.state
+    const { showServices, clickId, services, filter } = this.state
+    const visibleServices = getVisibleServices(services, filter)
     return (
       <div id='procedures_list'>
         <h2 className='procedures-title'>{config.translations.choose_service}</h2>
         <div className='search-strip'>
           <div className='search-wrap'>
-            <input className='search-input' value={this.state.search} onChange={e => this.search(e.target.value)} type='text' placeholder={config.translations.search_service} />
+            <input className='search-input' value={this.state.filter} onChange={this.handleFilterChange} type='text' placeholder={config.translations.search_service} />
             <img className='search-img' src={`${config.urls.media}magnifier.svg`} />
           </div>
         </div>
         <div className='procedure-wrap'>
-          {!this.props.isOpenServices && this.state.categories.map(i => <div key={i.id} data_id={i.id} className='extended-category' onClick={() => this.handleclickCategory(i.id)}><div className='category'>
+          {!this.props.isOpenServices && filter === '' ? this.state.categories.map(i => <div key={i.id} data_id={i.id} className='extended-category' onClick={() => this.handleclickCategory(i.id)}><div className='category'>
             <p className='category_name'>{i.name}</p>
             <p className='category_count'>({i.count})</p>
             <div className='icon_wrap'>
@@ -52,23 +59,18 @@ export default class ProceduresList extends React.Component {
             </div>
           </div>
             <div className={'acordeon ' + (showServices && clickId === i.id && 'active')}>
-              {this.state.services.filter(item => item.category.id === i.id).map(i => <div className='wrap-service'><div className='service' style={{borderColor: i.color}} onClick={() => this.props.getService(i)}>
-                <h2 className='name'>{i.name}</h2>
-                <div className='edditional-info'>
-                  <div className='dur-wrap'><img src={`${config.urls.media}ic_time.svg`} /><p className='duration'>{`${i.duration} ${config.translations.duration_minutes}`}</p></div>
-                  <div className='price-wrap'><img src={`${config.urls.media}debt.svg`} /><p className='price'>{`${i.price} ${config.data.currency}`}</p></div>
-                </div>
-              </div>
-              </div>)}
+              {this.state.services.filter(item => item.category.id === i.id).map(i => this.renderService(i))}
             </div>
-          </div>)}
+          </div>)
+          : <div>
+            {visibleServices.map(i => this.renderService(i))}
+          </div>}
         </div>
       </div>
     )
   }
 }
 ProceduresList.propTypes = {
-  toogleOpenServices: PropTypes.func,
   isOpenServices: PropTypes.bool,
   getService: PropTypes.func,
   data: PropTypes.arr
