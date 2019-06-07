157f353b07c24ab1ef543405396f1addf31b27c9
vipslord <my.yershov@gmail.com>
2019-06-07 19:13:13 +0300

    upd ProceduresList

--- ProceduresList/ProceduresList.jsx
+++ ProceduresList/ProceduresList.jsx
@@ -29,11 +29,14 @@ export default class ProceduresList extends React.Component {
   }
 
   renderService = item => (<div className='wrap-service'><div className='service' style={{borderColor: item.color}} onClick={() => this.props.getService(item)}>
-    <h2 className='name'>{item.name}</h2>
-    <div className='edditional-info'>
-      <div className='dur-wrap'><img src={`${config.urls.media}ic_time.svg`} /><p className='duration'>{`${item.duration} ${config.translations.duration_minutes}`}</p></div>
-      <div className='price-wrap'><p className='price'>{`${item.price} ${config.data.currency}`}</p></div>
+    <div className='common-wrap'>
+      <h2 className='name'>{item.name}</h2>
+      <div className='edditional-info'>
+        <div className='dur-wrap'><img src={`${config.urls.media}ic_time.svg`} /><p className='duration'>{`${item.duration} ${config.translations.duration_minutes}`}</p></div>
+        <div className='price-wrap'><p className='price'>{`${item.price} ${config.data.currency}`}</p></div>
+      </div>
     </div>
+    <img className='add-img' src={`${config.urls.media}plus_add.svg`} />
   </div>
   </div>
   )
@@ -51,20 +54,26 @@ export default class ProceduresList extends React.Component {
           </div>
         </div>
         <div className='procedure-wrap'>
-          {!this.props.isOpenServices && filter === '' ? this.state.categories.map(i => <div key={i.id} data_id={i.id} className='extended-category' onClick={() => this.handleclickCategory(i.id)}><div className='category'>
-            <p className='category_name'>{i.name}</p>
-            <p className='category_count'>({i.count})</p>
-            <div className='icon_wrap'>
-              <img className={config.isRTL ? 'rtl-arrow' : 'ltr-arrow'} src={config.urls.media + 'chevron-right.svg'} />
+          {!this.props.isOpenServices && filter === ''
+            ? this.state.categories.map(i => <div key={i.id} data_id={i.id} className={'extended-category' + (showServices && clickId === i.id && ' active_category')} onClick={() => this.handleclickCategory(i.id)}><div className=''>
+              <div className='category'>
+                <p className='category_name'>{i.name}</p>
+                <p className='category_count'>({i.count})</p>
+                <div className='icon_wrap'>
+                  <img className={config.isRTL
+                    ? 'rtl-arrow ' + (showServices && clickId === i.id && 'rotate-arrow')
+                    : 'ltr-arrow ' + (showServices && clickId === i.id && 'rotate-arrow')} src={config.urls.media + 'chevron-right.svg'} />
+                </div>
+              </div>
+              <div className='border' />
             </div>
-          </div>
             <div className={'acordeon ' + (showServices && clickId === i.id && 'active')}>
               {this.state.services.filter(item => item.category.id === i.id).map(i => this.renderService(i))}
             </div>
-          </div>)
-          : <div>
-            {visibleServices.map(i => this.renderService(i))}
-          </div>}
+            </div>)
+            : <div>
+              {visibleServices.map(i => this.renderService(i))}
+            </div>}
         </div>
       </div>
     )
