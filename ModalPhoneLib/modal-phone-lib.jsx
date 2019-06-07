6d2a6e5068863e3e64ea6bc92e440b491d31dd8d
DenisKenzo <denisamk44@gmail.com>
2019-06-07 16:11:34 +0300

    fixes in modalPhone

--- ModalPhoneLib/modal-phone-lib.jsx
+++ ModalPhoneLib/modal-phone-lib.jsx
@@ -34,9 +34,9 @@ export default class PhoneModal extends React.Component {
     this.props.closeModal()
   }
   save = () => {
-    if (this.props.reminders) {
-      this.props.create(this.state.inputValue)
-    } else {
+    if (this.props.saveNumber) { this.props.saveNumber(this.state.inputValue) }
+    else if (this.props.reminders) { this.props.create(this.state.inputValue) } 
+    else {
       config.data[config.urls.fields.phone] = this.state.inputValue
       this.props.create()
     }
@@ -52,16 +52,16 @@ export default class PhoneModal extends React.Component {
     return (
       <Modal show={this.props.visibleModal} onHide={this.back}>
         <div className='phone-modal-body'>
-          <img className={'close-bnt ' + (config.isRTL ? 'rtl' : 'ltr')} onClick={this.back} src={config.urls.media + 'btn-not.svg'} />
+          <img className={'close-bnt ' + (config.isRTL ? 'rtl-phone-modal' : 'ltr-phone-modal')} onClick={this.back} src={config.urls.media + 'btn-not.svg'} />
           <div className='phone-top'>
             <div className='picture'>
               <img src={config.urls.media + 'phone-off.svg'} />
             </div>
-            <div className='text-wrap'><p>{config.translations.not_found_phone_number}</p></div>
+            <div className='text-wrap'><p>{this.props.text.not_found_title}</p></div>
           </div>
           <div className='phone-bot'>
             <div className='phone-bot-text'>
-              <p>{config.translations.enter_phone_number}</p>
+              <p>{this.props.text.enter_phone_number}</p>
             </div>
             <div className='phone-input-wrap'>
               <div className='input-wrap'>
@@ -69,7 +69,7 @@ export default class PhoneModal extends React.Component {
                   type='tel'
                   ref='modal_phone'
                   value={this.state.inputValue}
-                  placeholder={config.translations.phone_number}
+                  placeholder={this.props.text.phone_number}
                   onChange={e => { this.checkPhone(e.target.value) }}
                 />
                 {this.state.isValidation &&
@@ -95,7 +95,7 @@ export default class PhoneModal extends React.Component {
             disabled={!validatePhone(this.state.inputValue)}
             onClick={this.save}>
             <div className='btns-wrap'>
-              {config.translations.save}
+              {this.props.text.save}
               <img src={config.urls.media + 'save.svg'} />
             </div>
           </button>
