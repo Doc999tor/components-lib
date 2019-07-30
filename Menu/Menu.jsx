729b149b974f043bd2909774672d76e6e396310a
vipslord <vipslord@gmail.com>
2019-07-30 18:52:03 +0300

    upd menu

--- Menu/Menu.jsx
+++ Menu/Menu.jsx
@@ -11,28 +11,31 @@ export default class Menu extends React.Component {
     }
     return (
       <div id='menu_modal' onClick={this.props.closeMenu}>
-        <div className='menu_wrap' style={bgrImg}>
-          <div className='list-wrap' onClick={e => e.stopPropagation()}>
-            <div className='menu'>
-              <div className='menu-logo'>
-                <div className='logo'>
-                  <img className='business_logo' src={config.user.business_logo} />
+        <div className='menu_container'>
+
+          <div className='menu_wrap' style={bgrImg}>
+            <div className='list-wrap' onClick={e => e.stopPropagation()}>
+              <div className='menu'>
+                <div className='menu-logo'>
+                  <div className='logo'>
+                    <img className='business_logo' src={config.user.business_logo} />
+                  </div>
+                  <p className='business_name'>{config.user.business_name}</p>
+                  <p className='business_address'><img className='business_address_img' src={`${config.urls.menu_icons}map-pin.svg`} />{config.user.business_address}</p>
                 </div>
-                <p className='business_name'>{config.user.business_name}</p>
-                <p className='business_address'><img className='business_address_img' src={`${config.urls.menu_icons}map-pin.svg`} />{config.user.business_address}</p>
+                <nav className='menu-list'>
+                  {config.menu.map(item => {
+                    return (
+                      <li className='list-item'>
+                        <a className='item-link' href={item.link}>
+                          <img className='menu-img' src={config.urls.menu_icons + item.icon} alt={item.text} />
+                          {config.translations.menu[item.text]}
+                        </a>
+                      </li>
+                    )
+                  })}
+                </nav>
               </div>
-              <nav className='menu-list'>
-                {config.menu.map(item => {
-                  return (
-                    <li className='list-item'>
-                      <a className='item-link' href={item.link}>
-                        <img className='menu-img' src={config.urls.menu_icons + item.icon} alt={item.text} />
-                        {config.translations.menu[item.text]}
-                      </a>
-                    </li>
-                  )
-                })}
-              </nav>
             </div>
           </div>
         </div>
