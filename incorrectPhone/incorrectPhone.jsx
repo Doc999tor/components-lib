c2cc99d8bf0fb6ce2ec3811dcadcf4a76dd79a31
vipslord <vipslord@gmail.com>
2019-08-05 16:50:53 +0300

    upd incorrectPhone modal

--- incorrectPhone/incorrectPhone.jsx
+++ incorrectPhone/incorrectPhone.jsx
@@ -1,22 +1,15 @@
 import { default as Modal } from 'project-components/Modal/Modal.jsx'
 // import './incorrectMail.styl'
 
-export default class IncorrectPhone extends React.Component {
-  static propTypes = {
-    closeMenu: PropTypes.func.isRequired
-  }
-
-  render () {
-    return (
-      <Modal show={this.props.show} onHide={this.props.onHide}>
-        <div className='modal-incorrect-phone'>
-          <img className='modal-icon' src={config.urls.media + 'alert-octagon.svg'} />
-          <label>{config.translations.personal_info_editing.title_incorrect_phone}</label>
-        </div>
-        <div className='modal-incorrect-phone-footer'>
-          <button onClick={this.props.onHide} className='no-btn'>{config.translations.personal_info_editing.agree}</button>
-        </div>
-      </Modal>
-    )
-  }
-}
+const IncorrectPhone = ({phone, onHide, show}) => (
+  <Modal show={show} onHide={onHide}>
+    <div className='modal-incorrect-phone'>
+      <img className='modal-icon' src={config.urls.media + 'alert-octagon.svg'} />
+      <label>{config.translations.personal_info_editing.title_incorrect_phone.replace('{phone_number}', phone)}</label>
+    </div>
+    <div className='modal-incorrect-phone-footer'>
+      <button onClick={onHide} className='no-btn'>{config.translations.personal_info_editing.agree}</button>
+    </div>
+  </Modal>
+)
+export default IncorrectPhone
