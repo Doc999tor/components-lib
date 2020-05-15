20cdd89020d6442c2e7366cc2dc0fc9a220bc866
tkachuks <serhiytkachuk88@gmail.com>
2020-05-15 17:34:45 +0300

    upd resize photo v1

--- resize.js
+++ resize.js
@@ -11,26 +11,13 @@ export default (photo, bar) => {
       let w = img.width
       let h = img.height
       if (w > config.data.max_side || h > config.data.max_side) {
-        if (w > h) {
-          if (w > config.data.max_side) {
-            h = (h * config.data.max_side) / w
-            w = config.data.max_side
-          }
-        } else {
-          if (h > config.data.max_side) {
-            w = (w * config.data.max_side) / h
-            h = config.data.max_side
-          }
-        }
+        w = (w * config.data.max_side) / h
+        h = config.data.max_side
       }
-      if (or > 4 && or < 9) {
-        canvas.width = h
-        canvas.height = w
-      } else {
-        canvas.width = w
-        canvas.height = h
-      }
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+      canvas.width = w
+      canvas.height = h
+
+      ctx.drawImage(img, 0, 0, w, h)
       let dataURL = canvas.toDataURL('image/jpeg', 0.8)
       bar(dataURL)
     }
