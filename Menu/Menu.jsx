715d8eac80295f049bd082aacf159db4bcd23656
tkachuks <serhiytkachuk88@gmail.com>
2020-06-11 17:20:10 +0300

    upd menu.jsx

--- Menu/Menu.jsx
+++ Menu/Menu.jsx
@@ -27,24 +27,26 @@ export default ({ closeMenu, commonConfig, translations }) => {
 	if (Object.entries(finalConfig.translations).length === 0 && finalConfig.translations.constructor === Object) {
 		finalConfig.translations = translations
 	}
-	const bgrImg = {
-		'background-image': `url('${finalConfig.urls.menu_icons}photo-bgr.jpg')`
-	}
-	const isSvg = finalConfig.user.business_logo.includes('.svg')
 	const cancelPropagation = e => e.stopPropagation()
 	return (
 		<div id='menu_modal' className='jsx-menu' onClick={closeMenu || false}>
 			<div className='menu_container'>
-				<div className='menu_wrap' style={bgrImg}>
+				<div className='menu_wrap'>
 					<div className='list-wrap' onClick={closeMenu ? cancelPropagation : false}>
 						<div className='menu'>
 							<div className='menu-logo'>
-								<div className={'logo' + (isSvg ? ' svg-logo' : '')}>
-									<img className='business_logo' src={finalConfig.user.business_logo} />
+								<div className='top-background'>
+									<img src={finalConfig.urls.menu_icons + 'wave_menu.svg'} alt='wave' />
+								</div>
+								<div className='logo'>
+									<img className='business_logo' src={finalConfig.urls.menu_icons + 'smart_manager.svg'} alt='smart_manager' />
 								</div>
 								<div className='business_container'>
-									<p className='business_name'>{finalConfig.user.business_name}</p>
-									<p className='business_address'><img className='business_address_img' src={`${finalConfig.urls.menu_icons}map-pin.svg`} />{finalConfig.user.business_address}</p>
+									<div className='neon-light'>
+										<div className='logo_wrap'>
+											<img className='app-logo' src={`${finalConfig.urls.menu_icons}logo.svg`} alt='logo' />
+										</div>
+									</div>
 								</div>
 							</div>
 							<nav className='menu-list'>
