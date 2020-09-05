a07de0498f1dacd26b6eb240afb66fa24d558d07
tkachuks <serhiytkachuk88@gmail.com>
2020-09-06 00:55:05 +0300

    minor upd for checkbox component

--- checkbox/checkbox.jsx
+++ checkbox/checkbox.jsx
@@ -5,7 +5,7 @@ import './checkbox.styl'
 export const Checkbox = ({ value, onHandleChange, text }) => (
   <label className='shared_checkbox_wrap'>
     <input className='shared_checkbox' type='checkbox'
-      value={value}
+      checked={value}
       onChange={onHandleChange}
     />
     <p className='shared_checkbox_label'>
