4d9b8a845ebad41f6cb537d36434af2e9bfdbb20
tkachuks <serhiytkachuk88@gmail.com>
2020-09-07 13:31:58 +0300

    added datepicker without moment.js

---
+++ Datepicker_upd/datepicker.jsx
@@ -0,0 +1,88 @@
+import './datepicker.styl'
+Date.prototype.daysInMonth = function() {
+  return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
+}
+export default class Datepicker extends React.Component {
+  state = {
+    min: '1930',
+    max: new Date().getFullYear()
+  }
+
+  renderOptionsYear = () => {
+    const arr = []
+    for (let i = this.state.max; i >= this.state.min; i--) {
+      arr.push(i)
+    }
+    return (
+      arr.map(opt => (
+        <option key={opt} value={opt}>
+          {opt}
+        </option>
+      ))
+    )
+  }
+
+  renderOptionsMonth = () => {
+    const arr = []
+    for (let index = 0; index < 12; index++) {
+      arr.push(config.translations.dates.months[index])
+    }
+    const res = arr.map((opt, index) => {
+      let indexStr = (index + 1) + ''
+      if (indexStr.length < 2) indexStr = 0 + indexStr
+      return (
+        <option key={opt} value={indexStr}>
+          {opt}
+        </option>
+      )
+    }
+    )
+    return res
+  }
+
+  renderOptionsDay = () => {
+    const arr = []
+    const month = this.props.month && !isNaN(+this.props.month) ? +this.props.month - 1 : 0
+    for (let i = 0; i < new Date(new Date().getFullYear(), month).daysInMonth(); i++) {
+      let day = (i + 1) + ''
+      if (day.length < 2) day = 0 + day
+      arr.push(day)
+    }
+    const res = arr.map((opt, index) => {
+      let indexStr = (index + 1) + ''
+      if (indexStr.length < 2) indexStr = 0 + indexStr
+      return (
+        <option key={opt} value={indexStr}>
+          {opt}
+        </option>
+      )
+    }
+    )
+    return res
+  }
+
+  render () {
+    return (
+      <div className='picker-wrap'>
+        <select className='year' value={this.props.year} onChange={this.props.handleChangeYear}>
+          <option value={config.translations.datepicker.placeholder.year} disabled>{config.translations.datepicker.placeholder.year}</option>
+          {
+            this.renderOptionsYear()
+          }
+        </select>
+        <select className='month' value={this.props.month} onChange={this.props.handleChangeMonth}>
+          <option value={config.translations.datepicker.placeholder.month} disabled>{config.translations.datepicker.placeholder.month}</option>
+          {
+            this.renderOptionsMonth()
+          }
+        </select>
+        <select className='day' value={this.props.day} onChange={this.props.handleChangeDay}>
+          <option value={config.translations.datepicker.placeholder.day} disabled>{config.translations.datepicker.placeholder.day}</option>
+          {
+            this.renderOptionsDay()
+          }
+        </select>
+      </div>
+    )
+  }
+}
