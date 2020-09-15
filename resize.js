c5a6a1c3d3ed0120fb4e97aeec336481752ed44c
tkachuks <serhiytkachuk88@gmail.com>
2020-09-15 17:14:40 +0300

    upd fix rotating photo in samsung browser

--- resize.js
+++ resize.js
@@ -1,25 +1,39 @@
 import { default as getOrientation } from 'project-components/getOrientation.js'
-export default (photo, bar) => {
+export default (photo, callback, highresPhotos = false) => {
   let img = new Image()
-  getOrientation(photo, or => {
+  getOrientation(photo, srcOrientation => {
     let reader = new FileReader()
     reader.readAsDataURL(photo)
     reader.onload = () => { img.src = reader.result }
     img.onload = () => {
       let canvas = document.createElement('canvas')
       let ctx = canvas.getContext('2d')
-      let w = img.width
-      let h = img.height
-      if (w > config.data.max_side || h > config.data.max_side) {
-        w = (w * config.data.max_side) / h
-        h = config.data.max_side
+      let width = img.width
+      let height = img.height
+      let userAgent = navigator.userAgent.toLowerCase()
+      let SamsungBrowser = /samsungbrowser/.test(userAgent)
+      if (4 < srcOrientation && srcOrientation < 9 && userAgent && SamsungBrowser) {
+        canvas.width = height
+        canvas.height = width
+      } else {
+        canvas.width = width
+        canvas.height = height
       }
-      canvas.width = w
-      canvas.height = h
-
-      ctx.drawImage(img, 0, 0, w, h)
-      let dataURL = canvas.toDataURL('image/jpeg', 0.8)
-      bar(dataURL)
+      if (userAgent && SamsungBrowser) {
+        switch (srcOrientation) {
+        case 2: ctx.transform(-1, 0, 0, 1, width, 0); break
+        case 3: ctx.transform(-1, 0, 0, -1, width, height); break
+        case 4: ctx.transform(1, 0, 0, -1, 0, height); break
+        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break
+        case 6: ctx.transform(0, 1, -1, 0, height, 0); break
+        case 7: ctx.transform(0, -1, -1, 0, height, width); break
+        case 8: ctx.transform(0, -1, 1, 0, 0, width); break
+        default: break
+        }
+      }
+      ctx.drawImage(img, 0, 0)
+      let dataURL = highresPhotos ? canvas.toDataURL('image/jpeg') : canvas.toDataURL('image/jpeg', 0.7)
+      callback(dataURL)
     }
   })
 }
