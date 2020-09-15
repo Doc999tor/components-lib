b39ec2b94584b57de0cb97f4eaf4b7a42b5be219
tkachuks <serhiytkachuk88@gmail.com>
2020-09-15 18:59:37 +0300

    upd save service(add-client)

--- save.js
+++ save.js
@@ -1,10 +1,10 @@
-export default (newPicture, newAgreement) => {
+export default (newPicture, newPictureName, newAgreement) => {
   let body = new FormData()
   let newAgr = newAgreement
   let newPic = newPicture
   const d = moment().format('YYYY-MM-DD HH:mm:ss')
   body.append('added', d)
-  newPicture && body.append('photo', newPic)
+  newPicture && body.append('photo', newPic, newPictureName)
   body.append('permit_ads', 'false')
   const checkFields = Object.keys(config.urls.fields).map(i => config.urls.fields[i])
   checkFields.forEach(i => {
