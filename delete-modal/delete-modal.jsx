87bbc6f48cb1a30ec7e25874439d751ee8b0e708
tkachuks <serhiytkachuk88@gmail.com>
2020-11-11 17:44:53 +0200

    upd delete-modal component

--- delete-modal/delete-modal.jsx
+++ delete-modal/delete-modal.jsx
@@ -5,6 +5,7 @@ const DeleteModal = ({
   onConfirmDeleteClients,
   closeDelete,
   confirmText,
+  staticImg,
   modalTitle,
   cancelText,
   modalText
@@ -15,14 +16,14 @@ const DeleteModal = ({
       <div className={'confirm-block' + (closeDelete ? ' confirm-block-hide' : '')} onClick={stopClick}>
         <div className='modal-body'>
           <p className='delete_title'>{modalTitle}</p>
-          <p class='text'>{modalText}</p>
+          <p className='text'>{modalText}</p>
           <div className='delete_icon'>
-            <img className='icon' src={config.urls.base_clients_list_data + 'ic_delete_popup.svg'} />
+            <img className='icon' src={(staticImg || config.urls.base_clients_list_data) + 'ic_delete_popup.svg'} />
           </div>
         </div>
         <div className='modal-footer'>
-          <button onClick={onCloseDeleteClientsModal} className='no-btn'><img class='cancel-img' src={config.urls.base_clients_list_data + 'ic_cancel_popup.svg'} />{cancelText}</button>
-          <button onClick={onConfirmDeleteClients} className='yes-btn'><img src={config.urls.base_clients_list_data + 'ic_delete.svg'} />{confirmText}</button>
+          <button onClick={onCloseDeleteClientsModal} className='no-btn'><img className='cancel-img' src={(staticImg || config.urls.base_clients_list_data) + 'ic_cancel_popup.svg'} />{cancelText}</button>
+          <button onClick={onConfirmDeleteClients} className='yes-btn'><img src={(staticImg || config.urls.base_clients_list_data) + 'ic_delete.svg'} />{confirmText}</button>
         </div>
       </div>
     </div>
