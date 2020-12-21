b8363dcd75732fd1341ef26e184dd90b0f19eef0
tkachuks <serhiytkachuk88@gmail.com>
2020-12-21 05:51:05 +0200

    upd sending popup

--- sending-popup/sending-popup.jsx
+++ sending-popup/sending-popup.jsx
@@ -5,6 +5,8 @@ const SendingPopup = ({
   isActivePopup,
   sending_label,
   success_label,
+  conflict_label,
+  conflictPopup = false
 }) => {
   return (
     <div className={'sending_popup' + (isActivePopup ? ' hide-background' : '')}>
@@ -14,12 +16,23 @@ const SendingPopup = ({
             <img className='plane' src={config.urls.media + 'ic_paper_plane.svg'} />
             <p>{sending_label}</p>
           </div>
-          : <div className='success'>
-            <div className='outer_circle'>
-              <img className='mark_modal' src={config.urls.media + 'ic_send_success.svg'} />
+          : !conflictPopup
+            ? <div className='success'>
+              <div className='outer_circle'>
+                <img className='mark_modal' src={config.urls.media + 'ic_send_success.svg'} />
+              </div>
+              <p>{success_label}</p>
             </div>
-            <p>{success_label}</p>
-          </div>}
+            : <div className='conflict'>
+              <div className='wrap_outer circle'>
+                <div className='wrap_inner circle'>
+                  <div className='wrap_center circle'>
+                    <img src={config.urls.media + 'ic_error.svg'} alt='' />
+                  </div>
+                </div>
+              </div>
+              <p>{conflict_label}</p>
+            </div>}
       </div>
     </div>
   )
