0d2346fc02ba2e015bab2fb2ad6749c4b8fc7bb2
tkachuks <serhiytkachuk88@gmail.com>
2021-02-03 14:17:43 +0200

    temp

--- parse_link_from_text.js
+++ parse_link_from_text.js
@@ -1,3 +1,8 @@
 export default text => {
-  return text.replace(/[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/gi, match => `<a class='parsed_link' target='_blank' href='${match}'>${match}</a>`)
+  console.log({ text });
+  // /[-a-zA-Z0-9@:%_+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_+.~#?&\/=\[\]]*)/gi - regex
+  return text.replace(/[-a-zA-Z0-9@:%_+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_+.~#?&\/=\[\]]*)/gi, match => {
+    console.log({ match });
+    return `<a class='parsed_link' target='_blank' href='${match}'>${match}</a>`
+  })
 }
