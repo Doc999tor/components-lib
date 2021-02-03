337c999b78e70b7cd0184482d20ff1a0582900cf
tkachuks <serhiytkachuk88@gmail.com>
2021-02-03 15:44:09 +0200

    parsing link from text

--- parse_link_from_text.js
+++ parse_link_from_text.js
@@ -1,8 +1,3 @@
 export default text => {
-  console.log({ text });
-  // /[-a-zA-Z0-9@:%_+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_+.~#?&\/=\[\]]*)/gi - regex
-  return text.replace(/[-a-zA-Z0-9@:%_+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_+.~#?&\/=\[\]]*)/gi, match => {
-    console.log({ match });
-    return `<a class='parsed_link' target='_blank' href='${match}'>${match}</a>`
-  })
+  return text.replace(/[-a-zA-Z0-9@:%_+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(?:[-a-zA-Z0-9@:%_+~#?&\/=\[\]]*)?/gi, match => `<a class='parsed_link' target='_blank' href='${match}'>${match}</a>`)
 }
