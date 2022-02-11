7df49f4de1ddb8ea1250c6d3ca02af60173ba3b3
doc999tor <doc999tor@gmail.com>
2022-02-11 21:30:25 +0200

    parse_link-regex-updated

--- parse_link_from_text.js
+++ parse_link_from_text.js
@@ -1,3 +1,3 @@
 export default text => {
-  return text.replace(/[-a-zA-Z0-9@:%_+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(?:[-a-zA-Z0-9@:%_+~#?&\/=\[\]]*)?/gi, match => `<a class='parsed_link' target='_blank' href='${match}'>${match}</a>`)
+  return text.replace(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(?:[.-a-zA-Z0-9@:%_+~#?&/=[\]]*)?/gi, match => `<a class='parsed_link' target='_blank' href='${match}'>${match}</a>`)
 }
